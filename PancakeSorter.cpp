2420075c046767ec7ca19a46beb6e8ecd21d3029
Jasmin C <jasmincho97@gmail.com>
2015-11-05 20:36:26 -0600

    Add difficulty buttons
    Ridiculously repetitive and long. Need to replace with vector of buttons
    later.

--- PancakeSorter.cpp
+++ PancakeSorter.cpp
@@ -6,6 +6,17 @@ PancakeSorter::PancakeSorter(Point xy, int w, int h, const string& title)
 	:Simple_window{xy,w,h,title}
 	{
 		attach(start);
+		attach(two);
+		attach(three);
+		attach(four);
+		attach(five);
+		attach(six);
+		attach(seven);
+		attach(eight);
+		attach(nine);
+		attach(ten);
+		attach(eleven);
+		attach(twelve);
 	}
 
 void PancakeSorter::cb_start(Address, Address window)
@@ -13,8 +24,114 @@ void PancakeSorter::cb_start(Address, Address window)
 	reference_to<PancakeSorter>(window).startGame();
 }
 
+void PancakeSorter::cb_two(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(2);
+}
+
+void PancakeSorter::cb_three(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(3);
+}
+
+void PancakeSorter::cb_four(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(4);
+}
+
+void PancakeSorter::cb_five(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(5);
+}
+
+void PancakeSorter::cb_six(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(6);
+}
+
+void PancakeSorter::cb_seven(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(7);
+}
+
+void PancakeSorter::cb_eight(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(8);
+}
+
+void PancakeSorter::cb_nine(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(9);
+}
+
+void PancakeSorter::cb_ten(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(10);
+}
+
+void PancakeSorter::cb_eleven(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(11);
+}
+
+void PancakeSorter::cb_twelve(Address, Address window)
+{
+	reference_to<PancakeSorter>(window).level(12);
+}
+
+void PancakeSorter::level(int lvl)
+{
+	switch(lvl)
+	{
+		case 2:
+			cout << "2 pancakes\n";
+			break;
+		case 3:
+			cout << "3 pancakes\n";
+			break;
+		case 4:
+			cout << "4 pancakes\n";
+			break;
+		case 5:
+			cout << "5 pancakes\n";
+			break;
+		case 6:
+			cout << "6 pancakes\n";
+			break;
+		case 7:
+			cout << "7 pancakes\n";
+			break;
+		case 8:
+			cout << "8 pancakes\n";
+			break;
+		case 9:
+			cout << "9 pancakes\n";
+			break;
+		case 10:
+			cout << "10 pancakes\n";
+			break;
+		case 11:
+			cout << "11 pancakes\n";
+			break;
+		case 12:
+			cout << "12 pancakes\n";
+			break;
+	}
+}
+
 void PancakeSorter::startGame()
 {
 	start.hide();
+	two.hide();
+	three.hide();
+	four.hide();
+	five.hide();
+	six.hide();
+	seven.hide();
+	eight.hide();
+	nine.hide();
+	ten.hide();
+	eleven.hide();
+	twelve.hide();
 }
 
--- PancakeSorter.h
+++ PancakeSorter.h
@@ -1,6 +1,5 @@
 //pancake sorter header
-//window/grpahics
-
+//window/graphics
 #include "Simple_window.h"
 #include "Graph.h"
 
@@ -9,8 +8,34 @@ struct PancakeSorter: Simple_window
 	PancakeSorter(Point(xy),int w,int h,const string& title);	
 
 	private:
-		Button start{Point{450,600},70,20,"START",cb_start};
+		Button start{Point{450,640},70,30,"START",cb_start};
+
+		Button two{Point{350,400},90,30,"2 Pancakes",cb_two};
+		Button three{Point{460,400},90,30,"3 Pancakes",cb_three};
+		Button four{Point{570,400},90,30,"4 Pancakes",cb_four};
+		Button five{Point{680,400},90,30,"5 Pancakes",cb_five};
+
+		Button six{Point{350,450},90,30,"6 Pancakes",cb_six};
+		Button seven{Point{460,450},90,30,"7 Pancakes",cb_seven};
+		Button eight{Point{570,450},90,30,"8 Pancakes",cb_eight};
+		Button nine{Point{680,450},90,30,"9 Pancakes",cb_nine};
+
+		Button ten{Point{350,500},90,30,"10 Pancakes",cb_ten};
+		Button eleven{Point{460,500},90,30,"11 Pancakes",cb_eleven};
+		Button twelve{Point{570,500},90,30,"12 Pancakes",cb_twelve};
 		
 		static void cb_start(Address, Address window);
 		void startGame();
+		static void cb_two(Address, Address window);
+		static void cb_three(Address, Address window);
+		static void cb_four(Address, Address window);
+		static void cb_five(Address, Address window);
+		static void cb_six(Address, Address window);
+		static void cb_seven(Address, Address window);
+		static void cb_eight(Address, Address window);
+		static void cb_nine(Address, Address window);
+		static void cb_ten(Address, Address window);
+		static void cb_eleven(Address, Address window);
+		static void cb_twelve(Address, Address window);
+		void level(int lvl);
 };
