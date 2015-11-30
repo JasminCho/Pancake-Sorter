428500e71015a2b5d3d00420400323ac40db7d71
Jasmin C <jasmincho97@gmail.com>
2015-11-29 21:23:50 -0600

    Fixed scoring again

--- Score.cpp
+++ Score.cpp
@@ -5,13 +5,13 @@
 
 void PancakeSorter::outputInitials()
 {
-	string player = playerInitials.get_string();
+	string player = getPlayerInitials();
 	cout << player << endl;
 }
 
 void PancakeSorter::newHiScore()
 {
-	HiScore myScore(playerInitials.get_string(), playerScore);
+	HiScore myScore(getPlayerInitials(), playerScore);
 	scores.push_back(myScore);
 
 	sortHiScores();
@@ -77,25 +77,41 @@ void PancakeSorter::setMoves(int newMoves)
 	movesText.set_label("Moves: " + to_string(moves));
 }
 
+vector<int>* PancakeSorter::getSolution()
+{
+	vector<int>* solution = m_find_solution(pancakePos);
+	return solution;
+}
+
+void PancakeSorter::calcMinMoves()
+{
+	vector<int>* solution = getSolution();
+	minMoves = num_moves(solution);
+}
+
+// For some stacks of more than 9 pancakes (like 1 2 3 4 5 6 10 9 8 7, with the 1 on top), this overestimates the minimum, so if the player actually gets the pancakes into order with fewer flips, add a “super bonus” of an extra 1000 points to the score with some message like “Wow!  You beat the computer!  You win 1000 extra bonus points!”  Use this test case to be sure your bonus works.
 void PancakeSorter::calcScore()
 {
 	if(pancakePos.size() > 0)
 	{
-		vector<int>* solution = m_find_solution(pancakePos);
-		int movesLeft = num_moves(solution);
-
-	 	playerScore = (100 - 10 * (moves - movesLeft)) * getNumPancakes();
+	 	playerScore = (100 - 10 * (moves - minMoves)) * getNumPancakes();
 		scoreText.set_label("Score: " + to_string(playerScore));
+	}
+}
 
-		if(playerScore <= 0)
-		{
-			attachEndScreen();
-			gameLose();
-		}
-		else if(movesLeft == 0)
-		{
-			attachEndScreen();
-			gameWin();
-		}
+void PancakeSorter::calcWinLose()
+{
+	vector<int>* solution = getSolution();
+	int movesLeft = num_moves(solution);
+
+	if(playerScore <= 0)
+	{
+		attachEndScreen();
+		gameLose();
+	}
+	else if(movesLeft == 0)
+	{
+		attachEndScreen();
+		gameWin();
 	}
 }
